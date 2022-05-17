20224db0764ccd265f66f8a156036e7b5168c085
mhulic <michal.hulic@niceactimize.com>
2022-05-17 08:42:08 +0200

    third branch

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
         int dlzkaHesla = sc.nextInt();
         boolean isValid = false;
 
-        String[] pole = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "/", "+", "%", "&", "@"};
+        String[] pole = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                "*", "/", "+", "%", "&", "@"};
+
         String[] shuffledArray = new String[pole.length];
         String[] heslo = new String[dlzkaHesla];
 
